1dc61eaa9bfa0a04fc8ff1cb803f9a3646da9c9c
nutes <lucasfstmd.projetos@gmail.com>
2023-05-31 15:43:33 -0300

    manutenção nos dtos

--- src/models/glicemia_controles/dto/update-glicemia_controle.dto.ts
+++ src/models/glicemia_controles/dto/update-glicemia_controle.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateGlicemiaControleDto } from './create-glicemia_controle.dto';
-import { IsDate, IsNumber, IsString } from 'class-validator';
+import { IsNumber, IsString } from 'class-validator';
 
 export class UpdateGlicemiaControleDto extends PartialType(
   CreateGlicemiaControleDto,
