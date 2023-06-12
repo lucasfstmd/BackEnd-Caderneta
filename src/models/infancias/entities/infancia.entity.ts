76595bd08dccc8aeac734e61395f4355ddd517b8
nutes <lucasfstmd.projetos@gmail.com>
2023-06-12 14:20:39 -0300

    ajuste na entidade Infancia

--- src/models/infancias/entities/infancia.entity.ts
+++ src/models/infancias/entities/infancia.entity.ts
@@ -14,73 +14,73 @@ export class Infancia {
   @Column({ nullable: false })
   paciente_id: number;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a1: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 250, nullable: true })
   a2_a: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a2_b: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 250, nullable: true })
   a3_a: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a3_b: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a4: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a5_a: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a5_b: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a5_c: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a5_d: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a5_e: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a5_f: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a5_g: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 250, nullable: true })
   a5_h: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a6: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   a7: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   b1: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   b2: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   b3_a: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   b3_b: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   b4: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   b5: string;
 
-  @Column({ nullable: true })
+  @Column({ length: 100, nullable: true })
   b6: string;
 
   @CreateDateColumn()
