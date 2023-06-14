b6c050a49402790a73e6c7fdb6fe79e82bb87597
Lucas Fausto Medeiros <irmaos3medeiros@gmail.com>
2023-06-14 09:54:13 -0300

    tentativa de deloy do banco de dados

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -6,11 +6,10 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: 'localhost',
-        port: 3306,
-        username: 'root',
-        password: 'root',
-        database: 'caderneta',
+        host: 'mysql://bda77cfb67a2d9:60cd6af0@us-cdbr-east-06.cleardb.net/heroku_cdcda3cf3bf842e?reconnect=true',
+        username: 'bda77cfb67a2d9',
+        password: '60cd6af0',
+        database: 'heroku_cdcda3cf3bf842e',
         synchronize: true,
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       });
