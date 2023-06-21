f2fcf956bf2a8f80a784c0916e30e606b98f7971
Lucas Fausto Medeiros <irmaos3medeiros@gmail.com>
2023-06-21 15:48:06 -0300

    Update database.controller.ts

--- src/models/database/database.controller.ts
+++ src/models/database/database.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { DatabaseService } from './database.service';
+
 @Controller('api/v1/database')
 export class DatabaseController {
   constructor(private readonly databaseService: DatabaseService) {}
