c5f2eaa76d2a3e25ba459bc6a542b2f7c7a106de
Lucas Fausto <lucasfstmd.projetos@gmail.com>
2024-06-05 15:55:34 -0300

    Adjustment Cors

--- src/main.ts
+++ src/main.ts
@@ -38,12 +38,19 @@ async function bootstrap() {
       }),
   );
   const port = process.env.PORT || 3333;
+
   app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
     next();
   });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*'
+  })
+
   await app.listen(port);
   console.log(`\nApplication on ${port}`);
 }
