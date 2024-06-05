78754bcdbe195af872f8e65f4cd8b3f63dee19be
Lucas Fausto <lucasfstmd.projetos@gmail.com>
2024-06-05 16:04:47 -0300

    Adjustment Cors

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
       .setVersion('1.0')
       .addTag('CRUDS')
       .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
@@ -38,18 +39,6 @@ async function bootstrap() {
       }),
   );
   const port = process.env.PORT || 3333;
-
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE, PATCH');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-
-  app.enableCors({
-    allowedHeaders: '*',
-    origin: '*'
-  })
 
   await app.listen(port);
   console.log(`\nApplication on ${port}`);
